fe552fe8add85eca7e0115997a7f9c2d4dcabba1
Chris Reeves <hello@chris.reeves.io>
2017-05-10 14:21:54 +0100

    Implment Post/Put HTTP request support

--- client.go
+++ client.go
@@ -36,7 +36,17 @@ func (c *Client) do(method, urlStr string, body io.Reader) (*http.Response, erro
 	return client.Do(req)
 }
 
-// Get sends a GET HTTP request for the given URL, no body is sent
+// Get sends a HTTP GET request for the given URL, no body is sent
 func (c *Client) Get(urlStr string) (*http.Response, error) {
 	return c.do(http.MethodGet, urlStr, nil)
+}
+
+// Post sends a HTTP POST request for the given url and request body
+func (c *Client) Post(urlStr string, body io.Reader) (*http.Response, error) {
+	return c.do(http.MethodPost, urlStr, body)
+}
+
+// Post sends a HTTP PUT request for the given url and request body
+func (c *Client) Put(urlStr string, body io.Reader) (*http.Response, error) {
+	return c.do(http.MethodPut, urlStr, body)
 }
