998be77115ec8ca0fadaf4f250c8797662a9c4ab
Chris Reeves <hello@chris.reeves.io>
2017-05-11 11:32:06 +0100

    Tests for parsing private key readers

---
+++ xero_test.go
@@ -0,0 +1,76 @@
+package xero
+
+import (
+	"bufio"
+	"bytes"
+	"crypto/rand"
+	"crypto/rsa"
+	"crypto/x509"
+	"encoding/pem"
+	"errors"
+	"io"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+type errReader struct {
+	err error
+}
+
+func (r errReader) Read([]byte) (int, error) {
+	return 0, r.err
+}
+
+func TestPrivateKey(t *testing.T) {
+	key, err := rsa.GenerateKey(rand.Reader, 512)
+	if err != nil {
+		t.Fatal(err)
+	}
+	var buff bytes.Buffer
+	wrtr := bufio.NewWriter(&buff)
+	if err := pem.Encode(wrtr, &pem.Block{
+		Type:  "PRIVATE KEY",
+		Bytes: x509.MarshalPKCS1PrivateKey(key),
+	}); err != nil {
+		t.Fatal(err)
+	}
+	wrtr.Flush()
+	parsedPk, err := x509.ParsePKCS1PrivateKey(x509.MarshalPKCS1PrivateKey(key))
+	if err != nil {
+		t.Fatal(err)
+	}
+	type testcase struct {
+		tname              string
+		r                  io.Reader
+		expectedPrivateKey *rsa.PrivateKey
+		expectedError      error
+	}
+	tt := []testcase{
+		testcase{
+			"read failure",
+			errReader{errors.New("read error")},
+			nil,
+			errors.New("read error"),
+		},
+		testcase{
+			"invalid pem",
+			bytes.NewReader([]byte("not a valid pk")),
+			nil,
+			errors.New("failed to decode PEM"),
+		},
+		testcase{
+			"valid pem",
+			bytes.NewReader(buff.Bytes()),
+			parsedPk,
+			nil,
+		},
+	}
+	for _, tc := range tt {
+		t.Run(tc.tname, func(t *testing.T) {
+			pk, err := PrivateKey(tc.r)
+			assert.Equal(t, tc.expectedPrivateKey, pk)
+			assert.Equal(t, tc.expectedError, err)
+		})
+	}
+}
